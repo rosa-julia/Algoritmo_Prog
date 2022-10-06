cd893db62e0e8cde7c12959450fc0ee279b7e304
Júlia Rosa <julia.britorosa@gmail.com>
2022-10-06 12:51:24 -0300

    Outro método
    outro método para fazer o switch case de meses igual no  documento "programaswitchcase"

---
+++ 1 semestre/Switch case/switchcase_mes.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+	
+	int mes;
+	printf("Digite o numero do mes que desejo conhecer: ");
+	scanf("%d", &mes); 
+	
+	if (mes == 1)
+		printf("Janeiro");
+		else if (mes == 2)
+			printf("Fevereiro");
+			else if (mes == 3)
+				printf("Março");
+					else if (mes == 4)
+						printf("Abril");
+						else if (mes == 5)
+							printf("Maio");
+							else if (mes == 6)
+								printf("Junho");
+								else if (mes == 7)
+									printf("Julho");
+									else if (mes == 8)
+										printf("Agosto");
+											else if (mes == 9)
+												printf("Setembro");
+												else if (mes == 10)
+													printf("Outubro");
+													else if (mes == 11)
+														printf("Novembro");
+														else if (mes == 12)
+															printf("Dezembro");
